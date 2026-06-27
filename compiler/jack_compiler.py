f4a3dead05c152beac4a83ad9d290d2bb64ee435
Magomed Makhmudov <mmriadovich@gmail.com>
2026-06-28 00:49:38 +0300

    commit

---
+++ compiler/jack_compiler.py
@@ -0,0 +1,25 @@
+import sys
+from tokenizer import tokenizer
+from pathlib import Path
+from parser import parser
+
+
+def main():
+    print(sys.argv)
+
+    path = sys.argv[1]
+
+    files = [f for f in Path(path).iterdir() if f.is_file()]
+
+    for file_path in files:
+        t = tokenizer()
+        t.open_file(file_path)
+        t.run()
+        t.generate_xml()
+
+        # p = parser(t.tokens, file_path)
+        # p.run()
+        # p.generate_xml()
+
+
+main()
